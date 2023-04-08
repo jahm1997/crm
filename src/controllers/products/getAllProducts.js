d75f47ab8482e107b37b9694f6e5c251bbe08bd0
jahm1997 <jahm1997@gmail.com>
2023-04-07 21:37:10 -0500

    modificaciones 9:37 7/4/23

--- src/controllers/products/getAllProducts.js
+++ src/controllers/products/getAllProducts.js
@@ -1,7 +1,5 @@
 const { where } = require("sequelize");
 const { Product } = require("../../db.js");
-const Buffer = require("buffer").Buffer;
-const base64 = require("base64-js");
 
 module.exports = async ({ bossId }) => {
   if (!bossId) throw new Error("bossId required");

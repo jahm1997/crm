29db2c935372f75c8a6c8ad02ae12dcd51392bed
jahm1997 <jahm1997@gmail.com>
2023-04-09 13:41:35 -0500

    1:41 om 9/4/23

--- src/controllers/salesman/customSalesman.js
+++ src/controllers/salesman/customSalesman.js
@@ -1,23 +1,36 @@
 const getMonthly_sales = require("../dashboard_salesman/getMonthly_sales");
 
-module.exports = async(salesmanDB) => {
-    const { id, name, address, email, phone, enable, createdAt, updatedAt, bossId, feedbacks } = salesmanDB.dataValues
-    const scores = feedbacks.map(f => f.dataValues.score)
-    const sum = scores.reduce((acc, curr) => acc + curr, 0);
-    const avgFeedback = sum / scores.length;
-    const {total_monthly_sales} = await getMonthly_sales(id)
-    return {
-        "id": id,
-        "name": name,
-        "address": address,
-        "email": email,
-        "phone": phone,
-        "enable": enable,
-        "createdAt": createdAt,
-        "updatedAt": updatedAt,
-        "bossId": bossId,
-        avgFeedback,
-        total_monthly_sales,
-        role:'seller'
-    }
-}
+module.exports = async (salesmanDB) => {
+  const {
+    id,
+    name,
+    address,
+    email,
+    phone,
+    enable,
+    image,
+    createdAt,
+    updatedAt,
+    bossId,
+    feedbacks,
+  } = salesmanDB.dataValues;
+  const scores = feedbacks.map((f) => f.dataValues.score);
+  const sum = scores.reduce((acc, curr) => acc + curr, 0);
+  const avgFeedback = sum / scores.length;
+  const { total_monthly_sales } = await getMonthly_sales(id);
+  return {
+    id: id,
+    name: name,
+    address: address,
+    email: email,
+    phone: phone,
+    enable: enable,
+    image: image,
+    createdAt: createdAt,
+    updatedAt: updatedAt,
+    bossId: bossId,
+    avgFeedback,
+    total_monthly_sales,
+    role: "seller",
+  };
+};

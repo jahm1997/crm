55a1e2077fdb8212429da79e6b7898924f7cf36e
jahm1997 <jahm1997@gmail.com>
2023-04-09 13:10:04 -0500

    modificacion gaspar

--- src/handlers/bossHandler.js
+++ src/handlers/bossHandler.js
@@ -26,6 +26,7 @@ const getBoss = async (req, res) => {
 //----------------------------------- HANDLERS POST -----------------------------------\\
 const postBoss = async (req, res) => {
   //data={name,username,email,password}
+  console.log(req.body);
   const data = JSON.parse(req.body.formLogin); //ALERT!!!!
   console.log(data);
   try {

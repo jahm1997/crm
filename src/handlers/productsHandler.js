87ab1207ce66947f71c23da448e9cc051836edc2
jahm1997 <jahm1997@gmail.com>
2023-04-09 10:34:15 -0500

    modify 10:34 9/4/23

--- src/handlers/productsHandler.js
+++ src/handlers/productsHandler.js
@@ -35,7 +35,7 @@ const postProduct = async (req, res) => {
 const putProduct = async (req, res) => {
   const data = JSON.parse(req.body.productData);
   try {
-    if (req.file.path) {
+    if (req.file.path !== null) {
       var response = await updateProduct(data, req.file.path);
     } else {
       var response = await updateProduct(data);

2128064ccb7aee3eb9f33180cffd24a4d262ab1e
jahm1997 <jahm1997@gmail.com>
2023-04-16 13:11:03 -0500

    edisiones

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ server.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Origin",
-    "https://crm-henry-34b.vercel.app" || "http://localhost:3000"
+    "http://localhost:3000" && "https://crm-henry-34b.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(

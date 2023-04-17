62fe6cfbd53e1eeebe807746e5ef6fe6af774662
jahm1997 <jahm1997@gmail.com>
2023-04-17 10:47:19 -0500

    veamos lowest

--- src/controllers/dashboard_boss/getLowest_stock.js
+++ src/controllers/dashboard_boss/getLowest_stock.js
@@ -2,24 +2,24 @@ const { Boss, Product } = require("../../db.js");
 const { where, Op } = require("sequelize");
 
 module.exports = async (id) => {
-    const allProducts = await Boss.findByPk(id,
-        {
-            include: [
-                {
-                    model: Product
-                }
-            ]
-        }
-    )
+  const allProducts = await Boss.findByPk(id, {
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  });
 
-    const products = allProducts.dataValues.products
-    let lowest_stock = products.map((p) => {
-        const { name, quantity, discount } = p.dataValues
-        return { name, quantity, discount }
-    })
+  console.log(allProducts);
 
-    lowest_stock.sort((x, y) => x.quantity - y.quantity)
-    lowest_stock = lowest_stock.slice(0, 10)
+  const products = allProducts.dataValues.products;
+  let lowest_stock = products.map((p) => {
+    const { name, quantity, discount } = p.dataValues;
+    return { name, quantity, discount };
+  });
 
-    return lowest_stock
-}
+  lowest_stock.sort((x, y) => x.quantity - y.quantity);
+  lowest_stock = lowest_stock.slice(0, 10);
+
+  return lowest_stock;
+};

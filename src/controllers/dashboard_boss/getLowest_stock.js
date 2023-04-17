c131dd210c32269c6b04b5f831034f864615a267
jahm1997 <jahm1997@gmail.com>
2023-04-17 10:51:01 -0500

    veamos lowest

--- src/controllers/dashboard_boss/getLowest_stock.js
+++ src/controllers/dashboard_boss/getLowest_stock.js
@@ -12,14 +12,18 @@ module.exports = async (id) => {
 
   console.log(allProducts);
 
-  const products = allProducts.dataValues.products;
-  let lowest_stock = products.map((p) => {
-    const { name, quantity, discount } = p.dataValues;
-    return { name, quantity, discount };
-  });
+  if (allProducts.length) {
+    const products = allProducts.dataValues.products;
+    let lowest_stock = products.map((p) => {
+      const { name, quantity, discount } = p.dataValues;
+      return { name, quantity, discount };
+    });
 
-  lowest_stock.sort((x, y) => x.quantity - y.quantity);
-  lowest_stock = lowest_stock.slice(0, 10);
+    lowest_stock.sort((x, y) => x.quantity - y.quantity);
+    lowest_stock = lowest_stock.slice(0, 10);
 
-  return lowest_stock;
+    return lowest_stock;
+  } else {
+    return [];
+  }
 };

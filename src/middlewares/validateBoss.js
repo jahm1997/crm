b62e2bc8c03c4aa01680ca83ee09325916887d8c
jahm1997 <jahm1997@gmail.com>
2023-04-19 13:36:27 -0500

    correción 400 create booss con cuenta existente

--- src/middlewares/validateBoss.js
+++ src/middlewares/validateBoss.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   const { email } = JSON.parse(req.body.formLogin);
   try {
     let exist = await Boss.findOne({ where: { email: email } });
-    if (exist !== null) res.status(200).json({ message: "El Jefe ya existe" });
+    if (exist !== null) res.status(400).json({ message: "El Jefe ya existe" });
     if (exist == null) next();
   } catch (error) {
     res.status(400).json({ error: error.message });

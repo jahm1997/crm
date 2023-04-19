4345de4735454573632f3e2dcd05f768500ed875
jahm1997 <jahm1997@gmail.com>
2023-04-19 16:37:55 -0500

    updatebos password

--- src/controllers/Bosses/CreateBoss.js
+++ src/controllers/Bosses/CreateBoss.js
@@ -12,9 +12,11 @@ const updateBoss = async (data, path) => {
     const dataAct = { ...data, logo };
     var id = dataAct.id;
     delete dataAct.id;
+    delete dataAct.createdAt;
+    delete dataAct.updatedAt;
     console.log("llegó aqui linea 15");
     if (data["password"]) {
-      dataAct.password = bcrypt.hashSync(password, 10);
+      dataAct.password = bcrypt.hashSync(data["password"], 10);
     }
     console.log("llegó aqui linea 19");
     var resultado = await Boss.update(dataAct, {
@@ -26,9 +28,12 @@ const updateBoss = async (data, path) => {
     const dataAct = { ...data };
     var id = dataAct.id;
     delete dataAct.id;
+    delete dataAct.createdAt;
+    delete dataAct.updatedAt;
     console.log("llegó aqui linea 29");
-    if (data["password"]) {
-      dataAct.password = bcrypt.hashSync(password, 10);
+    console.log("ESTO ES data-password", data["password"]);
+    if (data["password"].length < 25) {
+      dataAct.password = bcrypt.hashSync(data["password"], 10);
     }
     console.log("llegó aqui linea 33");
     var resultado = await Boss.update(dataAct, {

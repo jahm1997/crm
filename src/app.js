fef1dba67e0e621bf383803076a21db722291da0
jahm1997 <jahm1997@gmail.com>
2023-04-19 22:38:17 -0500

    correcion app

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ server.use((req, res, next) => {
   //   preflightContinue: true
   // })
 
-  res.header("Access-Control-Allow-Origin", "https://crm-henry-34b.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
